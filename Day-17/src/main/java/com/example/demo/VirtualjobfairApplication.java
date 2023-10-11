f4ec48b1b92b4b29c51af2323ccac1f553bec0db
Jenison <120012884+Jenison-07@users.noreply.github.com>
2023-10-11 14:05:36 +0530

    Add files via upload

---
+++ Day-17/src/main/java/com/example/demo/VirtualjobfairApplication.java
@@ -0,0 +1,15 @@
+ 
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualjobfairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualjobfairApplication.class, args);
+	}
+
+}
+
